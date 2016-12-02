121f564c3274c6bfb68966c4ffbc49b842d4f2f9
Mariano Salinas <ms8310@nyu.edu>
2016-12-01 19:45:05 -0500

    modified db

--- BookExchange/models/db.js
+++ BookExchange/models/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bookexchange');
+// mongoose.connect('mongodb://localhost/bookexchange');
+mongoose.connect('mongodb://ms8310:7kkB8vf3@class-mongodb.cims.nyu.edu/ms8310');
