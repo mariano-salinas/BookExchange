31af75818335ea2883cab44241bf27a39b36262e
Mariano Salinas <ms8310@nyu.edu>
2016-12-03 11:49:40 -0500

    updated urls

--- BookExchange/public/javascripts/add-posts.js
+++ BookExchange/public/javascripts/add-posts.js
@@ -1,10 +1,15 @@
+// var production ='http://i6.cims.nyu.edu:17368/api/post/create';
+// var local = 'http://localhost:3000/api/post/create';
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
 	var addPostBtn = document.querySelector('#addPost');
 	addPostBtn.addEventListener('click', function(evt){
 		addPost(evt);
-		window.location = "http://localhost:3000/home/posts";
+		window.location = "http://i6.cims.nyu.edu:17367/home/posts";
+
+		// window.location = "http://localhost:3000/home/posts";
 	});
 }
 function addPost(evt){
@@ -13,7 +18,7 @@ function addPost(evt){
 	var description = document.querySelector('#postDescription').value;
 
 	// var url = 'http://localhost:3000/api/post/create';
-	var url = 'http://i6.cims.nyu.edu:17368/api/post/create';
+	var url = 'http://i6.cims.nyu.edu:17367/api/post/create';
 	var req= new XMLHttpRequest();
 	req.open('POST', url, true);
 	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
